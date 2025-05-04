b1ce5f5f48308681fe1e0137ff97b500686b90ef
NiNiBeBeTo <141728273+NiNiBeBeTo@users.noreply.github.com>
2025-05-04 11:00:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'NiNi', // Your name
   ownerNumber: '2348036751970', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
